6902d68b00dee44fcb8e6742897d32340110fc68
oliverlowbob <oliv605m@stud.kea.dk>
2020-02-06 09:49:38 +0100

    dab

---
+++ 1.Intro/Exercises/basics.js
@@ -0,0 +1,9 @@
+var firstName = "Oliver";
+var lastName = "Dabberen";
+
+console.log("firstname:", firstName, "lastname:", lastName);
+
+var year = "2017";
+var number = 1;
+
+console.log(parseInt(year)+number); 
